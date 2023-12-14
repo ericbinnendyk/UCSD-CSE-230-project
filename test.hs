3ac4151fced13e523426d5ced1309c555e6f0d81
Eric B <ericabq@gmail.com>
2023-12-13 19:23:54 -0800

    add FWE-to-string conversion

--- test.hs
+++ test.hs
@@ -21,13 +21,17 @@ sub_fwe (FWE l1 h1) (FWE l2 h2) = add_fwe (FWE l1 h1) (neg_fwe (FWE l2 h2))
 
 neg_fwe (FWE l1 h1) = (FWE (-h1) (-l1))
 
+-- a simple floating point infinity, since apparently the keyword "Infinity" isn't built-in
+infinity :: Float
+infinity = 2.0**1024
+
 -- Multiplicative inverse, an auxiliary function used to define division
 -- I could give a special case for when the lower or upper bound of the range is exactly zero, and have the lower or upper bound of the range of the inverse be positive or negative infinity.
 -- But that would require checking whether one of the bounds of the range was exactly 0. We aren't supposed to do exact equality checks for floats.
 -- So I would need a different constructor for the FloatWithError type, that specifies that one of the bounds is *exactly* zero.
 inv_fwe (FWE l1 h1)
   | (l1 > 0 && h1 > 0) || (l1 < 0 && h1 < 0) = (FWE (1/h1) (1/l1))
-  | otherwise                                = (FWE (-Infinity) (Infinity)) -- technically this isn't the exact set of values it could take, but it's the minimal interval containing that set
+  | otherwise                                = (FWE (-infinity) (infinity)) -- technically this isn't the exact set of values it could take, but it's the minimal interval containing that set
 
 div_fwe (FWE l1 h1) (FWE l2 h2) = mul_fwe (FWE l1 h1) (inv_fwe (FWE l2 h2))
 
@@ -50,5 +54,8 @@ evalExpr expr =
     ExprNeg e1    -> neg_fwe (evalExpr e1)
     ExprNum x     -> x
 
+fweToString :: FloatWithError -> String
+fweToString (FWE low high) = show ((low + high) / 2) ++ "+/-" ++ show ((high - low) / 2)
+
 -- main = putStrLn (show (mul_fwe (add_fwe (FWE 0.5 0.9) (FWE 0.3 0.6)) (FWE (-0.2) 0.2)))
-main = putStrLn (show (evalExpr (ExprMul (ExprAdd (ExprNum (FWE 0.5 0.9)) (ExprNum (FWE 0.3 0.6))) (ExprNum (FWE (-0.2) 0.2)))))
+main = putStrLn (fweToString (evalExpr (ExprMul (ExprAdd (ExprNum (FWE 0.5 0.9)) (ExprNum (FWE 0.3 0.6))) (ExprNum (FWE (-0.2) 0.2)))))
